efe18c294ea3927e77ecd9f7b3dd4ff080e51e8a
aurora32s <aurora32s@naver.com>
2022-07-27 01:16:52 +0900

    feat : bottom navigation item 생성 #2

--- app/src/main/java/com/seom/accountbook/AccountDestination.kt
+++ app/src/main/java/com/seom/accountbook/AccountDestination.kt
@@ -7,3 +7,25 @@ interface AccountDestination {
     val icon: Int // icon drawable id
     val route: String // navigation destination title
 }
+
+object History : AccountDestination {
+    override val icon = R.drawable.ic_history
+    override val route = "history"
+}
+
+object Calendar : AccountDestination {
+    override val icon = R.drawable.ic_calendar
+    override val route = "calendar"
+}
+
+object Graph : AccountDestination {
+    override val icon = R.drawable.ic_graph
+    override val route = "graph"
+}
+
+object Setting : AccountDestination {
+    override val icon = R.drawable.ic_settings
+    override val route = "setting"
+}
+
+val accountBottomTabScreens = listOf(History, Calendar, Graph, Setting)
