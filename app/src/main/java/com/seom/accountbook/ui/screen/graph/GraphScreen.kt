a88be8c684ed75a21944a87a529cf9df818a6acb
aurora32s <aurora32s@naver.com>
2022-07-30 04:54:40 +0900

    feat : graph 화면에 date app bar 추가 #16

--- app/src/main/java/com/seom/accountbook/ui/screen/graph/GraphScreen.kt
+++ app/src/main/java/com/seom/accountbook/ui/screen/graph/GraphScreen.kt
@@ -1,9 +1,34 @@
 package com.seom.accountbook.ui.screen.graph
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.seom.accountbook.ui.components.DateAppBar
+import com.seom.accountbook.ui.screen.calendar.CalendarContainer
+import com.seom.accountbook.ui.screen.calendar.RowData
+import com.seom.accountbook.ui.theme.ColorPalette
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun GraphScreen() {
-    Text(text = "graph")
+    DateAppBar(
+        onDateChange = {
+
+        },
+        children = {
+            Column {
+                Divider(
+                    color = ColorPalette.Purple,
+                    thickness = 1.dp
+                )
+            }
+        }
+    )
 }
