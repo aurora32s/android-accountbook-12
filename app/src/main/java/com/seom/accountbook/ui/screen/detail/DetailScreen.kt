fa9075bb0623f396b9260675076fef81a6757cf8
aurora32s <aurora32s@naver.com>
2022-07-30 19:00:19 +0900

    feat : 하단의 일별 지출 내역 표현해주기(history의 리스트 composable 참고) #18

--- app/src/main/java/com/seom/accountbook/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/seom/accountbook/ui/screen/detail/DetailScreen.kt
@@ -2,18 +2,16 @@ package com.seom.accountbook.ui.screen.detail
 
 import android.graphics.Paint
 import android.graphics.Typeface
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -22,14 +20,20 @@ import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.seom.accountbook.R
+import com.seom.accountbook.mock.histories
 import com.seom.accountbook.model.BaseCount
 import com.seom.accountbook.model.category.Category
 import com.seom.accountbook.model.graph.OutComeByMonth
+import com.seom.accountbook.model.history.History
+import com.seom.accountbook.model.history.HistoryType
 import com.seom.accountbook.model.method.Method
 import com.seom.accountbook.ui.components.OneButtonAppBar
+import com.seom.accountbook.ui.screen.history.HistoryList
+import com.seom.accountbook.ui.screen.history.HistoryListHeader
 import com.seom.accountbook.ui.screen.post.PostBody
 import com.seom.accountbook.ui.screen.post.PostTopTab
 import com.seom.accountbook.ui.theme.ColorPalette
@@ -115,6 +119,10 @@ fun DetailScreen(
                 color = ColorPalette.LightPurple,
                 thickness = 1.dp
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutComeList(
+                historyGroupedByDate = histories
+            )
         }
     }
 }
@@ -197,6 +205,124 @@ fun LinearGraph(
                         color = if (index == data.size - 1) primaryTextColor else secondaryTextColor
                     )
                 )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun OutComeList(
+    // TODO Key 를 String 으로 두는게 맞을까...
+    historyGroupedByDate: Map<String, List<History>>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        historyGroupedByDate.forEach { (date, histories) ->
+            stickyHeader {
+                HistoryListHeader(
+                    date = date,
+                    income = histories.filter { it.type == HistoryType.INCOME }
+                        .sumOf { it.money },
+                    outCome = histories.filter { it.type == HistoryType.OUTCOME }
+                        .sumOf { it.money }
+                )
+            }
+            items(items = histories) { history ->
+                OutComeListItem(
+                    history = history
+                )
+            }
+            item {
+                Divider(
+                    color = ColorPalette.LightPurple,
+                    thickness = 1.dp
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun OutComeListItem(
+    history: History,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Transparent)
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 8.dp
+            )
+    ) {
+        Divider(
+            color = ColorPalette.Purple40,
+            thickness = 1.dp
+        )
+        Row(
+            modifier = Modifier.padding(top = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.padding(start = 16.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = history.categoryName,
+                        modifier = Modifier
+                            .widthIn(56.dp)
+                            .clip(RoundedCornerShape(999.dp))
+                            .background(Color(history.categoryColor))
+                            .padding(
+                                start = 8.dp,
+                                top = 4.dp,
+                                bottom = 4.dp,
+                                end = 8.dp
+                            ),
+                        style = MaterialTheme.typography.subtitle2,
+                        color = ColorPalette.White,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = history.method,
+                        style = MaterialTheme.typography.subtitle1,
+                        color = ColorPalette.Purple,
+                        textAlign = TextAlign.End,
+                        modifier = Modifier
+                            .padding(top = 4.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = history.content,
+                        style = MaterialTheme.typography.caption,
+                        color = ColorPalette.Purple
+                    )
+                    Text(
+                        text = "${
+                            if (history.type == HistoryType.OUTCOME) -1 * history.money
+                            else history.money
+                        } 원",
+                        style = MaterialTheme.typography.body2,
+                        fontWeight = FontWeight(700),
+                        color = if (history.type == HistoryType.INCOME) ColorPalette.Green else ColorPalette.Red
+                    )
+                }
             }
         }
     }
