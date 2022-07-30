350121cc2431f85db7732ed7a375677db6d2e568
aurora32s <aurora32s@naver.com>
2022-07-30 20:43:24 +0900

    feat : 상단 app bar 생성 #20

--- app/src/main/java/com/seom/accountbook/ui/screen/setting/SettingScreen.kt
+++ app/src/main/java/com/seom/accountbook/ui/screen/setting/SettingScreen.kt
@@ -1,9 +1,39 @@
 package com.seom.accountbook.ui.screen.setting
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.seom.accountbook.ui.theme.ColorPalette
 
 @Composable
 fun SettingScreen() {
-    Text(text = "setting")
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "설정",
+                    style = MaterialTheme.typography.body1.copy(color = ColorPalette.Purple)
+                )
+            }
+        }
+    ) {
+        Column {
+            Divider(
+                color = ColorPalette.Purple,
+                thickness = 1.dp
+            )
+        }
+    }
 }
