9a5a8587f82e9367378835c3dd69b2bf01adffbb
aurora32s <aurora32s@naver.com>
2022-07-30 22:20:39 +0900

    feat : 결제 수단 / 카테고리 추가 화면에 이름 입력란 추가 #20

--- app/src/main/java/com/seom/accountbook/ui/screen/setting/category/CategoryAddScreen.kt
+++ app/src/main/java/com/seom/accountbook/ui/screen/setting/category/CategoryAddScreen.kt
@@ -2,13 +2,21 @@ package com.seom.accountbook.ui.screen.setting.category
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.seom.accountbook.R
 import com.seom.accountbook.model.history.HistoryType
 import com.seom.accountbook.ui.components.OneButtonAppBar
+import com.seom.accountbook.ui.screen.setting.method.Input
+import com.seom.accountbook.ui.screen.setting.method.InputField
+import com.seom.accountbook.ui.theme.ColorPalette
 
 @Composable
 fun CategoryAddScreen(
@@ -19,6 +27,8 @@ fun CategoryAddScreen(
     val title = if (categoryType == HistoryType.INCOME) "수입" else "지출"
     val modeTitle = if (categoryId.isNullOrBlank()) "추가하기" else "수정하기"
 
+    var name by remember { mutableStateOf("") }
+
     Scaffold(topBar = {
         OneButtonAppBar(title = "$title 카테고리 $modeTitle") {
             Image(
@@ -27,6 +37,14 @@ fun CategoryAddScreen(
                 modifier = Modifier.clickable { onBackButtonPressed() })
         }
     }) {
-
+        Box {
+            Divider(
+                color = ColorPalette.Purple,
+                thickness = 1.dp
+            )
+            InputField(title = "이름") {
+                Input(content = name, onValueChange = { name = it })
+            }
+        }
     }
 }
--- app/src/main/java/com/seom/accountbook/ui/screen/setting/method/MethodAddScreen.kt
+++ app/src/main/java/com/seom/accountbook/ui/screen/setting/method/MethodAddScreen.kt
@@ -2,12 +2,18 @@ package com.seom.accountbook.ui.screen.setting.method
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.seom.accountbook.R
 import com.seom.accountbook.ui.components.OneButtonAppBar
+import com.seom.accountbook.ui.theme.ColorPalette
 
 @Composable
 fun MethodAddScreen(
@@ -15,6 +21,8 @@ fun MethodAddScreen(
     onBackButtonPressed: () -> Unit
 ) {
     val modeTitle = if (methodId.isNullOrBlank()) "추가하기" else "수정하기"
+    var name by remember { mutableStateOf("") }
+
     Scaffold(topBar = {
         OneButtonAppBar(title = "결제 수단 $modeTitle") {
             Image(
@@ -23,6 +31,71 @@ fun MethodAddScreen(
                 modifier = Modifier.clickable { onBackButtonPressed() })
         }
     }) {
+        Box {
+            Divider(
+                color = ColorPalette.Purple,
+                thickness = 1.dp
+            )
+            InputField(title = "이름") {
+                Input(content = name, onValueChange = { name = it })
+            }
+        }
+    }
+}
 
+@Composable
+fun InputField(
+    title: String,
+    input: @Composable () -> Unit
+) {
+    Column(modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 16.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    top = 8.dp,
+                    bottom = 8.dp
+                )
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.caption,
+                fontWeight = FontWeight(500),
+                color = ColorPalette.Purple,
+                modifier = Modifier.weight(2f)
+            )
+            Surface(
+                modifier = Modifier
+                    .weight(8f)
+                    .padding(start = 8.dp),
+                color = Color.Transparent
+            ) {
+                input()
+            }
+        }
+        Divider(
+            color = ColorPalette.Purple40,
+            thickness = 1.dp
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
+
+@Composable
+fun Input(
+    content: String,
+    onValueChange: (String) -> Unit
+) {
+    Box() {
+        BasicTextField(
+            value = content,
+            onValueChange = { onValueChange(it) },
+            textStyle = MaterialTheme.typography.caption.copy(color = ColorPalette.Purple),
+        )
+        if (content.isBlank()) {
+            Text(
+                text = "입력하세요.",
+                style = MaterialTheme.typography.caption.copy(color = ColorPalette.LightPurple)
+            )
+        }
     }
 }
